17b9e89fb892728b703e539dcb2dc1aaafdd6264
Коломыцев Андрей <akolomytsev@softrust.ru>
2024-11-08 21:45:06 +0300

    les_2_2: Выполненное задание

---
+++ src/main/kotlin/les_2_2/les_2_2.kt
@@ -0,0 +1,27 @@
+package les_2_2
+
+fun main() {
+    val employeeCount = 50
+    val internCount = 30
+
+    println("Кол-во постоянных сотрудников: $employeeCount")
+    println("Кол-во постоянных стажёров: $internCount")
+
+    val employeeSalary = 30000
+    val internSalary = 20000
+
+    println("Зп постоянных сотрудников: $employeeSalary")
+    println("Зп постоянных стажёров: $internSalary")
+
+    val onlyEmployeePayments = employeeCount * employeeSalary
+
+    println("Выплаты только сотрудникам: $onlyEmployeePayments")
+
+    val totalPayments = onlyEmployeePayments + internCount * internSalary
+
+    println("Общая сумма выплат: $totalPayments")
+
+    val averageSalary = totalPayments / (employeeCount + internCount)
+
+    println("Средняя зп: $averageSalary")
+}
