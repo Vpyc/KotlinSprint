9a7111ff1e13612750c039b35f0d6815c5c9151e
Коломыцев Андрей <akolomytsev@softrust.ru>
2024-11-08 20:44:47 +0300

    les_1_3: Выполненное задание

---
+++ src/main/kotlin/les_3/les3.kt
@@ -0,0 +1,16 @@
+package les_3
+
+fun main() {
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    println("$hour:$minute")
+}
