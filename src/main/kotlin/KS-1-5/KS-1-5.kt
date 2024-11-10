b0ff3e0556a1c8abdb4c42c6639e4209f0af96ef
Коломыцев Андрей <akolomytsev@softrust.ru>
2024-11-10 20:24:12 +0300

    KS-1-5-Поменял переменную на константу

--- src/main/kotlin/KS-1-5/KS-1-5.kt
+++ src/main/kotlin/KS-1-5/KS-1-5.kt
@@ -1,12 +1,14 @@
 package `KS-1-5`
 
+const val SECONDS_MINUTES_DIVIDER = 60
+
 fun main() {
-    val secondsMinutesDivider = 60
+
     val flightSeconds = 6480
 
-    val hour = flightSeconds / secondsMinutesDivider / secondsMinutesDivider
-    val minutes = (flightSeconds - (hour * secondsMinutesDivider * secondsMinutesDivider)) / secondsMinutesDivider
-    val remainsSeconds = (flightSeconds - (hour * secondsMinutesDivider * secondsMinutesDivider)) - (minutes * secondsMinutesDivider)
+    val hour = flightSeconds / SECONDS_MINUTES_DIVIDER / SECONDS_MINUTES_DIVIDER
+    val minutes = (flightSeconds - (hour * SECONDS_MINUTES_DIVIDER * SECONDS_MINUTES_DIVIDER)) / SECONDS_MINUTES_DIVIDER
+    val remainsSeconds = (flightSeconds - (hour * SECONDS_MINUTES_DIVIDER * SECONDS_MINUTES_DIVIDER)) - (minutes * SECONDS_MINUTES_DIVIDER)
 
     println("%02d:%02d:%02d".format(hour, minutes, remainsSeconds))
 }
