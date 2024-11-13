376085f6e0b601dc04b680ed505c3405ea8d812f
Коломыцев Андрей <akolomytsev@softrust.ru>
2024-11-13 19:02:25 +0300

    KS-3-4-Выполнил поставленное задание

---
+++ src/main/kotlin/KS-3-4/KS-3-4.kt
@@ -0,0 +1,15 @@
+package `KS-3-4`
+
+fun main() {
+    var startPosition = "E2"
+    var finalPosition = "E4"
+    var turn = 1
+
+    println("$startPosition-$finalPosition;$turn")
+
+    startPosition = "D2"
+    finalPosition = "D3"
+    turn = 3
+
+    println("$startPosition-$finalPosition;$turn")
+}
