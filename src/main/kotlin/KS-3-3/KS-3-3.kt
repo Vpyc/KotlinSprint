907948736a55e0d2feb621eebc27f87c85db806f
Коломыцев Андрей <akolomytsev@softrust.ru>
2024-11-13 18:45:07 +0300

    KS-3-3-Выполнил поставленное задание

---
+++ src/main/kotlin/KS-3-3/KS-3-3.kt
@@ -0,0 +1,16 @@
+package `KS-3-3`
+
+fun main() {
+    val variable = 523123123213253133L
+
+    println("""
+        $variable X 1 = ${variable * 1}
+        $variable X 2 = ${variable * 2}
+        $variable X 3 = ${variable * 3}
+        $variable X 4 = ${variable * 4}
+        $variable X 5 = ${variable * 6}
+        $variable X 7 = ${variable * 7}
+        $variable X 8 = ${variable * 8}
+        $variable X 9 = ${variable * 9}
+    """.trimIndent())
+}
